b780e5b2013358bdb9b247f2e4cfd77d3339378e
Soul <mixalex205@gmail.com>
2025-06-19 09:49:51 +0300

    color depends on the system 1.1.1

--- app/src/main/java/com/financeproject/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/financeproject/ui/screens/SplashScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
